e0844bebd9219eb40142fb6c77d040a4e20003c1
Tom Dunn <tidunn13@gmail.com>
2013-07-12 02:19:35 -0400

    Added search functionality

--- static/js/draw_graph.js
+++ static/js/draw_graph.js
@@ -17,8 +17,44 @@ function init() {
 
     sigInst.parseGexf('static/data/communities.gexf');
     sigInst.draw();
-    //sigInst.startForceAtlas2();
+
+    window.searchSubs = function(term) {
+        currentSearchTerm = term = term.toLowerCase();
+
+        var found = false;
+        sigInst.iterNodes(function(node) {
+
+            if (node.label.toLowerCase() == term) {
+                node.active = true;
+            }
+
+        });
+
+        sigInst.position(0,0,1).draw();
+    };
+
+    window.clearSearch = function() {
+        sigInst.iterNodes(function(node) {
+            node.active = false;
+        });
+
+        sigInst.position(0,0,1).draw();
+    };
 }
+
+$('#search-graph').bind('keydown', function(e) {
+    if (e.which !== 13) return;
+
+    var term = $(e.target).val().toLowerCase();
+    if (term.length == 0) return;
+    clearSearch();
+    searchSubs(term);
+});
+
+$('#clear-search').click(function(e) {
+    $('#search-graph').val('');
+    clearSearch();
+});
 
 if (document.addEventListener) {
     document.addEventListener("DOMContentLoaded", init, false);
