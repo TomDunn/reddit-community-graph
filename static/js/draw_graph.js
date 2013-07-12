a45b7422263c1be6f55bdea7ad38d723c7411fc3
Tom Dunn <tidunn13@gmail.com>
2013-07-12 15:39:37 -0400

    Added more filtering functionality

--- static/js/draw_graph.js
+++ static/js/draw_graph.js
@@ -17,71 +17,8 @@ function init() {
 
     sigInst.parseGexf('static/data/communities.gexf');
     sigInst.draw();
-
-    sigInst.bind('upnodes', function(e) {
-        window.open('http://reddit.com/r/' + sigInst.getNodes(e.content[0]).label, '_blank');
-    });
-
-    var currentSearchTerm = '';
-    window.searchSubs = function(term) {
-        currentSearchTerm = term = term.toLowerCase();
-
-        var found = false;
-        sigInst.iterNodes(function(node) {
-
-            if (node.label.toLowerCase() == term) {
-                node.active = true;
-            }
-
-        });
-
-        sigInst.position(0,0,1).draw();
-    };
-
-    window.clearSearch = function() {
-        sigInst.iterNodes(function(node) {
-            node.active = false;
-        });
-
-        sigInst.position(0,0,1).draw();
-    };
-
-    sigInst.bind('overnodes', function(e) {
-        var nodes = e.content;
-        var neighbors = {};
-        sigInst.iterEdges(function(edge) {
-            if(nodes.indexOf(edge.source) >= 0 || nodes.indexOf(edge.target) >= 0) {
-                neighbors[edge.source] = 1;
-                neighbors[edge.target] = 1;
-            }
-        }).iterNodes(function(n) {
-            if(neighbors[n.id]) {
-                n.active = true;
-            }
-        }).draw(2,2,2);
-    });
-
-    sigInst.bind('outnodes', function(e) {
-        sigInst.iterNodes(function(node) {
-            if (node.label == currentSearchTerm) return;
-            node.active = false
-        }).draw(2,2,2);
-    });
+    extend_graph(sigInst);
 }
-
-$('#search-graph').bind('keydown', function(e) {
-    if (e.which !== 13) return;
-
-    var term = $(e.target).val().toLowerCase();
-    if (term.length == 0) return;
-    clearSearch();
-    searchSubs(term);
-});
-
-$('#clear-search').click(function(e) {
-    $('#search-graph').val('');
-    clearSearch();
-});
 
 if (document.addEventListener) {
     document.addEventListener("DOMContentLoaded", init, false);
---
+++ static/js/extend_graph.js
@@ -0,0 +1,125 @@
+function extend_graph(sigInst) {
+
+    var panel = $('#sub-control-panel');
+    panel.hide();
+    $('#close-panel').click(function(e) {
+        e.preventDefault();
+        panel.hide();
+    });
+
+    var linkContainer = $('#sub-link');
+    var selectedNode = '';
+
+    sigInst.bind('upnodes', function(e) {
+        selectedNode = sigInst.getNodes(e.content[0]);
+        linkContainer.html('<a target="_blank" href="http://reddit.com/r/' + selectedNode.label + '">r/' + selectedNode.label + '</a>');
+        panel.show();
+    });
+
+    var activateConnectedNodesBox = $('#activate-connected-nodes');
+    var hideEdgesBox = $('#hide-edges');
+    var hideNodesBox = $('#hide-nodes');
+
+    var showConnectedNodes = false;
+    var hideEdges = false;
+    var hideNodes = false;
+
+    activateConnectedNodesBox.change(function() {
+        if (this.checked) {
+            var neighbors = {};
+            sigInst.iterEdges(function(e) {
+
+                if(selectedNode.id == e.source || selectedNode.id == e.target) {
+                    neighbors[e.source] = 1;
+                    neighbors[e.target] = 1;
+                }
+
+            }).iterNodes(function(n) {
+
+                if(neighbors[n.id]) {
+                    n.active = true;
+                }
+
+            }).draw(2,2,2);
+        } else {
+            sigInst.iterNodes(function(n) {
+                if (n.label == currentSearchTerm) return;
+                n.active = false
+            }).draw(2,2,2);
+        }
+    });
+
+    hideEdgesBox.change(function() {
+        if (this.checked) {
+            sigInst.iterEdges(function(e) {
+                if (selectedNode.id != e.source && selectedNode.id != e.target) {
+                    e.hidden = true;
+                }
+            }).draw(2,2,2);
+        } else {
+            sigInst.iterEdges(function(e) {
+                e.hidden = false;
+            }).draw(2,2,2);
+        }
+    });
+
+    hideNodesBox.change(function() {
+        if (this.checked) {
+            var neighbors = {};
+            sigInst.iterEdges(function(e) {
+                if(selectedNode.id == e.source || selectedNode.id == e.target) {
+                    neighbors[e.source] = 1;
+                    neighbors[e.target] = 1;
+                }
+
+            }).iterNodes(function(n) {
+
+                if(!neighbors[n.id]) {
+                    n.hidden = true;
+                }
+
+            }).draw(2,2,2);
+        } else { 
+            sigInst.iterNodes(function(n) {
+                n.hidden = false;
+            }).draw(2,2,2);
+        }
+    });
+
+    var currentSearchTerm = '';
+    window.searchSubs = function(term) {
+        currentSearchTerm = term = term.toLowerCase();
+
+        var found = false;
+        sigInst.iterNodes(function(node) {
+
+            if (node.label.toLowerCase() == term) {
+                node.active = true;
+            }
+
+        });
+
+        sigInst.position(0,0,1).draw();
+    };
+
+    window.clearSearch = function() {
+        currentSearchTerm = '';
+        sigInst.iterNodes(function(node) {
+            node.active = false;
+        }).draw(2,2,2);
+    };
+
+    $('#search-graph').bind('keydown', function(e) {
+        if (e.which !== 13) return;
+
+        var term = $(e.target).val().toLowerCase();
+        if (term.length == 0) return;
+        clearSearch();
+        searchSubs(term);
+    });
+
+    $('#clear-search').click(function(e) {
+        $('#search-graph').val('');
+        clearSearch();
+    });
+}
